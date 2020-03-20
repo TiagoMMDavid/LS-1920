b4c30e49c3c6b8c65a05bfd54447aebd8ead2b44
Tiago David <A46078@alunos.isel.pt>
2020-03-20 20:55:02 +0000

    Added more methods to GetCommandsTest.java

--- src/test/java/pt/isel/ls/model/commands/GetCommandsTest.java
+++ src/test/java/pt/isel/ls/model/commands/GetCommandsTest.java
@@ -3,11 +3,23 @@ package pt.isel.ls.model.commands;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import pt.isel.ls.model.Router;
+import pt.isel.ls.model.commands.common.CommandHandler;
+import pt.isel.ls.model.commands.common.CommandRequest;
+import pt.isel.ls.model.commands.common.CommandResult;
+import pt.isel.ls.model.commands.common.Method;
+import pt.isel.ls.model.paths.Path;
+import pt.isel.ls.model.paths.PathTemplate;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static pt.isel.ls.model.commands.common.PsqlConnectionHandler.getConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Iterator;
 
 public class GetCommandsTest {
 
@@ -66,5 +78,68 @@ public class GetCommandsTest {
     @Test
     public void GetBookingsByRoomAndBookingIdTest() {
         //TODO:
+    }
+
+    @Test
+    public void GetBookingsByUserIdTest() {
+        //TODO:
+    }
+
+    @Test
+    public void GetLabelsTest() {
+        //TODO:
+    }
+
+    @Test
+    public void GetRoomsByIdTest() {
+        Router router = new Router();
+        router.addRoute(Method.GET, new PathTemplate("/rooms/{rid}"), new GetRoomsByIdCommand());
+        CommandRequest cmd = new CommandRequest(Method.GET, new Path("/rooms/0"));
+
+        CommandHandler handler = router.findRoute(cmd.getMethod(), cmd.getPath());
+        CommandResult result = handler.execute(cmd);
+        Iterator<String> itr = result.iterator();
+
+        assertNotNull(result);
+        assertTrue(result.isSuccess());
+        assertEquals(itr.next(), "room id (rid): 0");
+        assertEquals(itr.next(), "name: Meeting Room");
+    }
+
+    @Test
+    public void GetRoomsTest() {
+        Router router = new Router();
+        router.addRoute(Method.GET, new PathTemplate("/rooms"), new GetRoomsCommand());
+        CommandRequest cmd = new CommandRequest(Method.GET, new Path("/rooms"));
+
+        CommandHandler handler = router.findRoute(cmd.getMethod(), cmd.getPath());
+        CommandResult result = handler.execute(cmd);
+        Iterator<String> itr = result.iterator();
+
+        assertNotNull(result);
+        assertTrue(result.isSuccess());
+        assertEquals(itr.next(), "Meeting Room (rid: 0)");
+        assertEquals(itr.next(), "Snack Room (rid: 1)");
+    }
+
+    @Test
+    public void GetRoomsWithLabelTest() {
+        //TODO:
+    }
+
+    @Test
+    public void GetUsersByIdTest() {
+        Router router = new Router();
+        router.addRoute(Method.GET, new PathTemplate("/users/{uid}"), new GetUsersByIdCommand());
+        CommandRequest cmd = new CommandRequest(Method.GET, new Path("/users/0"));
+
+        CommandHandler handler = router.findRoute(cmd.getMethod(), cmd.getPath());
+        CommandResult result = handler.execute(cmd);
+        Iterator<String> itr = result.iterator();
+
+        assertNotNull(result);
+        assertTrue(result.isSuccess());
+        assertEquals(itr.next(), "user id (uid): 0");
+        assertEquals(itr.next(), "name: John Frank");
     }
 }
