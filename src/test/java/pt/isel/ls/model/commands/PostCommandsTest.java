ef096ad7053ed9545b24f0fdcde69c368e25cded
PTKickass <jp_moura99@hotmail.com>
2020-03-24 18:57:05 +0000

    Implemented first PostCommands tests

--- src/test/java/pt/isel/ls/model/commands/PostCommandsTest.java
+++ src/test/java/pt/isel/ls/model/commands/PostCommandsTest.java
@@ -3,11 +3,18 @@ package pt.isel.ls.model.commands;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import pt.isel.ls.model.Router;
+import pt.isel.ls.model.commands.common.*;
+import pt.isel.ls.model.paths.Path;
+import pt.isel.ls.model.paths.PathTemplate;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Iterator;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static pt.isel.ls.model.commands.common.PsqlConnectionHandler.getConnection;
 
 //TODO:
@@ -15,7 +22,15 @@ public class PostCommandsTest {
 
     @Test
     public void postBookingsInRoomCommandTest() {
+        Router router = new Router();
+        router.addRoute(Method.POST, new PathTemplate("/rooms/{rid}/bookings"), new PostBookingsInRoomCommand());
+        CommandRequest cmd = new CommandRequest(Method.POST, new Path("/rooms/0/bookings"), new Parameters("begin=2020-12-20&duration=10&uid=1"));
 
+        CommandHandler handler = router.findRoute(cmd.getMethod(), cmd.getPath());
+        CommandResult result = handler.execute(cmd);
+
+        assertNotNull(result);
+        assertTrue(result.isSuccess());
     }
 
     @Test
@@ -30,7 +45,15 @@ public class PostCommandsTest {
 
     @Test
     public void PostUsersCommandTest() {
+        Router router = new Router();
+        router.addRoute(Method.POST, new PathTemplate("/users"), new PostUsersCommand());
+        CommandRequest cmd = new CommandRequest(Method.POST, new Path("/users"), new Parameters("name=David&email=Paul"));
 
+        CommandHandler handler = router.findRoute(cmd.getMethod(), cmd.getPath());
+        CommandResult result = handler.execute(cmd);
+
+        assertNotNull(result);
+        assertTrue(result.isSuccess());
     }
 
     @BeforeClass
